Add core test coverage and fix masking and positional encoding issues (#5)
* fix: tweak positional encoding bug

* Adding coverage as a dev dependency

* fix: Adding a fix for the masking

* feat: Adding testing for attention, encoding and data handling

embeddings/positional_encoding.py
@@ -36,6 +36,6 @@ class PositionalEncoding(nn.Module):
 
     def forward(self, x):
         # x is of shape (batch_size, seq_len, d_model)
-        batch_size, seq_len, _ = x.size()
+        _, seq_len, _ = x.size()
 
-        return x + self.pe[:seq_len, :]
+        return x + self.pe[:, :seq_len, :]